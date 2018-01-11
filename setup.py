52310fad4c0ba98605bc5bae3562ab5ce7449aa3
red-coracle <red-coracle@users.noreply.github.com>
2018-01-11 12:54:03 -0500

    Version 0.1.2.2  * Update argon2-cffi version  * Update copyright year

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 '''
-    Flask-Argon2
-    ------------
+Flask-Argon2
+------------
 
-    The Flask-Argon2 extension provides Argon2i password hashing with
-    sensible defaults and a random salt for Flask.
+The Flask-Argon2 extension provides Argon2i password hashing with
+sensible defaults and a random salt for Flask.
 
-    Links
-    `````
-    * `Documentation <http://flask-argon2.readthedocs.org/en/latest/>`_
-    * `Development version <https://github.com/red-coracle/flask-argon2>`_
-    * `argon2_cffi <https://github.com/hynek/argon2_cffi>`_
+Links
+`````
+* `Documentation <http://flask-argon2.readthedocs.org/en/latest/>`_
+* `Development version <https://github.com/red-coracle/flask-argon2>`_
+* `argon2_cffi <https://github.com/hynek/argon2_cffi>`_
 '''
 
 import os
